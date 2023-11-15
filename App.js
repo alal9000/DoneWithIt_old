3a75c0d8e5576b346ac8742d85b9b07c8cd39df1
Aaron Lal <lucidlear@gmail.com>
2023-11-15 20:45:59 +1100

    button

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  TouchableNativeFeedback,
-  View,
-  Image,
-  SafeAreaView
-} from "react-native";
+import { Button, SafeAreaView, StyleSheet } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native</Text>
-      <TouchableNativeFeedback onPress={() => console.log("image tapped")}>
-        <View
-          style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}
-        ></View>
-      </TouchableNativeFeedback>
+      <Button
+        color="orange"
+        title="click me"
+        onPress={() => console.log("tapped")}
+      />
     </SafeAreaView>
   );
 }
