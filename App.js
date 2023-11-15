4ebe9ff78e348b668d0ec08a15552ce7ef04be96
Aaron Lal <lucidlear@gmail.com>
2023-11-15 20:06:18 +1100

    text

--- App.js
+++ App.js
@@ -2,9 +2,15 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("txt pressed");
+
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native</Text>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello React Native - A really really long text. Now I want to make this
+        even longer and see what happens
+      </Text>
     </SafeAreaView>
   );
 }
