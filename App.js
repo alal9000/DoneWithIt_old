0b6afff754e2fca2fc2c4f87388e5b8b06776783
Aaron Lal <lucidlear@gmail.com>
2023-11-16 16:02:59 +1100

    flexbox - flexWrap and alignContent

--- App.js
+++ App.js
@@ -8,27 +8,42 @@ export default function App() {
         flex: 1,
         flexDirection: "row", // horizontal
         justifyContent: "center", // main
-        alignItems: "center" // secondary
+        alignItems: "center", // secondary
+        alignContent: "center",
+        flexWrap: "wrap"
       }}
     >
       <View
         style={{
           backgroundColor: "dodgerblue",
           width: 100,
-          height: 300,
-          alignSelf: "flex-start"
+          height: 300
         }}
       />
       <View
         style={{
           backgroundColor: "gold",
           width: 100,
-          height: 200
+          height: 100
         }}
       />
       <View
         style={{
           backgroundColor: "tomato",
+          width: 100,
+          height: 100
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "grey",
+          width: 100,
+          height: 100
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "greenyellow",
           width: 100,
           height: 100
         }}
