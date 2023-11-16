4e79ed32f683287372f374159fa8f2484bbf360d
Aaron Lal <lucidlear@gmail.com>
2023-11-16 16:12:13 +1100

    flexbox - flexBasis, flexGrow and flexShrink

--- App.js
+++ App.js
@@ -10,19 +10,20 @@ export default function App() {
         justifyContent: "center", // main
         alignItems: "center", // secondary
         alignContent: "center",
-        flexWrap: "wrap"
       }}
     >
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width: 100,
-          height: 300
+          width: 400,
+          flex: -1,
+          height: 100
         }}
       />
       <View
         style={{
           backgroundColor: "gold",
+          flexBasis: 100,
           width: 100,
           height: 100
         }}
@@ -30,20 +31,6 @@ export default function App() {
       <View
         style={{
           backgroundColor: "tomato",
-          width: 100,
-          height: 100
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "grey",
-          width: 100,
-          height: 100
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "greenyellow",
           width: 100,
           height: 100
         }}
