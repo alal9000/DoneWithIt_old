49be3ff7847bf5886661968279b6bdd5cd09f8d7
Aaron Lal <lucidlear@gmail.com>
2023-11-17 11:38:37 +1100

    platform specific code

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { View } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AppText from "./app/components/AppText";
 
 export default function App() {
   return (
@@ -10,7 +10,7 @@ export default function App() {
         alignItems: "center"
       }}
     >
-      <MaterialCommunityIcons name="email" size={200} color="dodgerblue" />
+      <AppText>I love react native</AppText>
     </View>
   );
 }
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -5,10 +5,21 @@ function AppText({ children }) {
   return <Text style={styles.text}>{children}</Text>;
 }
 
+
+
 const styles = StyleSheet.create({
   text: {
-    fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir"
+    color: "tomato",
+    ...Platform.select({
+      ios: {
+        fontSize: 20,
+        fontFamily: "Avenir"
+      },
+      android: {
+        fontSize: 18,
+        fontFamily: "Roboto"
+      }
+    })
   }
 });
 
