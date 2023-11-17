0addfae45128b50b4a8478fa51808708caedc84b
Aaron Lal <lucidlear@gmail.com>
2023-11-17 19:41:31 +1100

    improving the welcome screen

--- app/components/AppButton.js
+++ app/components/AppButton.js
@@ -2,9 +2,12 @@ import { TouchableOpacity } from "react-native";
 import colors from "../config/colors";
 import { StyleSheet, Text } from "react-native";
 
-export default function AppButton({ title, onPress }) {
+export default function AppButton({ title, onPress, color = "primary" }) {
   return (
-    <TouchableOpacity style={styles.button} onPress={onPress}>
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor: colors[color] }]}
+      onPress={onPress}
+    >
       <Text style={styles.text}>{title}</Text>
     </TouchableOpacity>
   );
@@ -17,6 +20,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 15,
+    marginVertical: 10,
     width: "100%"
   },
   text: {
