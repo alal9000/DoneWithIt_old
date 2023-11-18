09190c3d52f777fa5f9194d6b4e58c2dd4d8fd85
Aaron Lal <lucidlear@gmail.com>
2023-11-18 16:43:52 +1100

    TextInput

--- App.js
+++ App.js
@@ -1,5 +1,24 @@
+import React, { useState } from "react";
+
+import { Text, TextInput } from "react-native";
+import Screen from "./app/components/Screen";
 import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
-  return <ListingsScreen />;
+  const [firstName, setFirstName] = useState("");
+
+  return (
+    <Screen>
+      <Text>{firstName}</Text>
+      <TextInput
+        secureTextEntry
+        onChangeText={(text) => setFirstName(text)}
+        placeholder="First Name"
+        style={{
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1
+        }}
+      />
+    </Screen>
+  );
 }
