797a7cdac5c0ed5d4fde7a93fdbcdcca8b539150
Aaron Lal <lucidlear@gmail.com>
2023-11-19 07:53:55 +1100

    building the login form

--- App.js
+++ App.js
@@ -1,29 +1,11 @@
 import { useState } from "react";
+import LoginScreen from "./app/screens/LoginScreen";
 
-import Screen from "./app/components/Screen";
-import { Switch } from "react-native";
-import AppPicker from "./app/components/AppPicker";
-import AppTextInput from "./app/components/AppTextInput";
 
-const categories = [
-  { label: "Furniture", value: 1 },
-  { label: "Clothing", value: 2 },
-  { label: "Cameras", value: 3 }
-];
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
 
   return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        icon="apps"
-        placeholder="Category"
-        items={categories}
-      />
-      <AppTextInput icon="email" placeholder="Email" />
-    </Screen>
+    <LoginScreen />
   );
 }
