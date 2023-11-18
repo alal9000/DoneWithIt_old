aadf2e2a75ce30ed862fe44061c84c1a33223baa
Aaron Lal <lucidlear@gmail.com>
2023-11-18 14:33:04 +1100

    extending the ListItem component

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
 
 export default function App() {
   return (
     <Screen>
-      <Icon name="email" size={100} />
+      <ListItem
+        title="My title"
+        // subTitle="My subtitle"
+        ImageComponent={<Icon name="email" />}
+      />
     </Screen>
   );
 }
