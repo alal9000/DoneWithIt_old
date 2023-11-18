c0c8dcdbe153042a3d5cf2f7737bc596fe66465a
Aaron Lal <lucidlear@gmail.com>
2023-11-18 19:49:32 +1100

    showing picker items

--- App.js
+++ App.js
@@ -5,12 +5,18 @@ import { Switch } from "react-native";
 import AppPicker from "./app/components/AppPicker";
 import AppTextInput from "./app/components/AppTextInput";
 
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 }
+];
+
 export default function App() {
   const [isNew, setIsNew] = useState(false);
 
   return (
     <Screen>
-      <AppPicker icon="apps" placeholder="Category" />
+      <AppPicker icon="apps" placeholder="Category" items={categories} />
       <AppTextInput icon="email" placeholder="Email" />
     </Screen>
   );
