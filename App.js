e9fa04fb98196afb2fd448140f78ef216afb14c2
Aaron Lal <lucidlear@gmail.com>
2023-11-18 17:46:00 +1100

    building a pretty textInput

--- App.js
+++ App.js
@@ -1,24 +1,14 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-import { Text, TextInput } from "react-native";
+import AppTextInput from "./app/components/AppTextInput";
 import Screen from "./app/components/Screen";
-import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
   const [firstName, setFirstName] = useState("");
 
   return (
     <Screen>
-      <Text>{firstName}</Text>
-      <TextInput
-        secureTextEntry
-        onChangeText={(text) => setFirstName(text)}
-        placeholder="First Name"
-        style={{
-          borderBottomColor: "#ccc",
-          borderBottomWidth: 1
-        }}
-      />
+      <AppTextInput placeholder="Username" icon="email" />
     </Screen>
   );
 }
---
+++ app/components/AppTextInput.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { TextInput, View, StyleSheet } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import colors from "../config/colors";
+
+function AppTextInput({ icon, ...otherProps }) {
+  return (
+    <View style={styles.container}>
+      {icon && (
+        <MaterialCommunityIcons
+          name={icon}
+          size={20}
+          color={colors.medium}
+          style={styles.icon}
+        />
+      )}
+      <TextInput {...otherProps} style={styles.textInput} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.light,
+    borderRadius: 25,
+    flexDirection: "row",
+    width: "100%",
+    padding: 15,
+    marginVertical: 10
+  },
+  icon: {
+    marginRight: 10,
+    marginVertical: 5
+  },
+  textInput: {
+    color: colors.dark,
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir"
+  }
+});
+
+export default AppTextInput;
