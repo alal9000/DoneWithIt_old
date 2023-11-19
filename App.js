cc5141f8d84850d8a58afb7e26c345acc8a5db13
Aaron Lal <lucidlear@gmail.com>
2023-11-19 15:50:55 +1100

    handling long text

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-import ListItem from "./app/components/lists/ListItem";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-
 import Screen from './app/components/Screen'
 import MessagesScreen from "./app/screens/MessagesScreen";
 
