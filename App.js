2ddf60f6554af3f46245ecf326b5233ec10a710c
Aaron Lal <lucidlear@gmail.com>
2023-11-20 12:06:49 +1100

    requesting permissions and permissions module

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import Screen from "./app/components/Screen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+import { useEffect } from "react";
 
 export default function App() {
-  return (
-    <Screen>
-      <ListingEditScreen />
-    </Screen>
-  );
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted) alert("You need to enable permission to access the library");
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  return <Screen></Screen>;
 }
