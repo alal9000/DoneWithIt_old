19ac8cb78b892ff4b9d86b5227e56a87a9a352c3
Aaron Lal <lucidlear@gmail.com>
2023-11-22 19:56:52 +1100

    building ImageInputList basics and scrolling

--- App.js
+++ App.js
@@ -1,36 +1,24 @@
+import { useState } from "react";
+import ImageInputList from "./app/components/ImageInputList";
 import Screen from "./app/components/Screen";
-import * as ImagePicker from "expo-image-picker";
-import * as Permissions from "expo-permissions";
-import { useEffect, useState } from "react";
-import { Button, Image } from "react-native";
-import ImageInput from "./app/components/ImageInput";
 
 export default function App() {
-  const [imageUri, setImageUri] = useState(null);
+  const [imageUris, setImageUris] = useState([]);
 
-  const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (!granted) alert("You need to enable permission to access the library");
+  const handleAdd = (uri) => {
+    setImageUris([...imageUris, uri]);
   };
 
-  useEffect(() => {
-    requestPermission();
-  }, []);
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.canceled) setImageUri(result.uri);
-    } catch (error) {
-      console.log("error reading an image", error);
-    }
+  const handleRemove = (uri) => {
+    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
   };
 
   return (
     <Screen>
-      <ImageInput
-        imageUri={imageUri}
-        onChangeImage={(uri) => setImageUri(uri)}
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
       />
     </Screen>
   );
