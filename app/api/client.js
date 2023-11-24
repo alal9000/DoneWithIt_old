54061b61b7e6053fbbd88a7bc260b4149e6b4b6c
Aaron Lal <lucidlear@gmail.com>
2023-11-24 19:36:09 +1100

    creating an API layer

--- app/api/client.js
+++ app/api/client.js
@@ -1,11 +1,7 @@
-import { create } from 'apisauce'
+import { create } from "apisauce";
 
 const apiClient = create({
-  baseURL: 'http://159.196.169.142:5000/api'
+  baseURL: "http://159.196.169.142:5000/api"
 });
 
-apiClient.get('/listings').then(response => {
-  if (!response.ok) {
-    response.problem
-  }
-})
+export default apiClient;
