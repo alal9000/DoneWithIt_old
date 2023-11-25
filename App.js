8ec40b6e9c4d7757818a270a93d92f4d3c1539c9
Aaron Lal <lucidlear@gmail.com>
2023-11-25 18:33:59 +1100

    AsyncStorage

--- App.js
+++ App.js
@@ -2,9 +2,21 @@ import React from "react";
 import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
 import { View } from "react-native";
 import { Button } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
-  const netInfo = useNetInfo();
+  const demo = async () => {
+    try {
+      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }));
+      const value = await AsyncStorage.getItem("person");
+      const person = JSON.parse(value);
+      console.log(person);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  return <Button disabled={!netInfo.isInternetReachable} />
+  demo();
+
+  return null;
 }
