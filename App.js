f0c3d0a9c17cf0b6d7a0ea30474dfc642d4d9b12
Aaron Lal <lucidlear@gmail.com>
2023-11-25 15:57:40 +1100

    detecting network status

--- App.js
+++ App.js
@@ -1,85 +1,10 @@
 import React from "react";
-import { Text } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-
+import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
+import { View } from "react-native";
 import { Button } from "react-native";
-import Screen from "./app/components/Screen";
-import AppNavigator from "./app/navigation/AppNavigator";
-import navigationTheme from "./app/navigation/navigationTheme";
-import AuthNavigator from "./app/navigation/authNavigator";
-
-const Link = () => {
-  const navigation = useNavigation();
-
-  return (
-    <Button title="Click" onPress={() => navigation.navigate("TweetDetails")} />
-  );
-};
-
-const Tweets = ({ navigation }) => (
-  <Screen>
-    <Text>Tweets</Text>
-    <Button
-      title="View Tweet"
-      onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
-    />
-  </Screen>
-);
-
-const TweetDetails = ({ route }) => (
-  <Screen>
-    <Text>Tweet Details {route.params.id}</Text>
-  </Screen>
-);
-
-const Stack = createStackNavigator();
-const FeedNavigator = () => (
-  <Stack.Navigator
-    screenOptions={{
-      headerStyle: { backgroundColor: "dodgerblue" },
-      headerTintColor: "white",
-      headerTitleAlign: "center"
-    }}
-  >
-    <Stack.Screen
-      name="Tweets"
-      component={Tweets}
-      options={{
-        headerStyle: { backgroundColor: "tomato" },
-        headerTintColor: "white",
-        headerTitleAlign: "center",
-        headerShown: false
-      }}
-    />
-    <Stack.Screen
-      name="TweetDetails"
-      component={TweetDetails}
-      options={({ route }) => ({ title: "Tweet Details" })}
-    />
-  </Stack.Navigator>
-);
-
-const AccountNavigator = () => (
-  <Screen>
-    <Text>Account</Text>
-  </Screen>
-);
-
-const Tab = createBottomTabNavigator();
-const TabNavigator = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Feed" component={FeedNavigator} />
-    <Tab.Screen name="Account" component={AccountNavigator} />
-  </Tab.Navigator>
-);
 
 export default function App() {
-  return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
-  );
+  const netInfo = useNetInfo();
+
+  return <Button disabled={!netInfo.isInternetReachable} />
 }
